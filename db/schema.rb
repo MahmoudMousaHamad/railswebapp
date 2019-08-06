ac27df3672c22fb50db0aabb1ac032ebaef7d496
Mousa Hamad <mahmoudhamad@ou.edu>
2019-08-06 17:01:33 -0500

    Commiting to deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_07_29_151609) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
