102c1273d215c59c997c31624cd78a6c722f7f85
Mousa Hamad <mahmoudhamad@ou.edu>
2019-08-14 17:10:02 -0500

    .

--- db/migrate/20190810163242_create_scholarships.rb
+++ db/migrate/20190810163242_create_scholarships.rb
@@ -1,5 +1,14 @@
 class CreateScholarships < ActiveRecord::Migration[5.2]
   def change
+    create_table :universities do |t|
+      t.string :name
+      t.text :about
+      t.references :city, foreign_key: true
+      t.references :country, foreign_key: true
+
+      t.timestamps
+    end
+
     create_table :scholarships do |t|
       t.string :name
       t.text :about
--- db/migrate/20190810163650_create_universities.rb
+++ db/migrate/20190810163650_create_universities.rb
@@ -1,12 +1,12 @@
 class CreateUniversities < ActiveRecord::Migration[5.2]
   def change
-    create_table :universities do |t|
-      t.string :name
-      t.text :about
-      t.references :city, foreign_key: true
-      t.references :country, foreign_key: true
+    # create_table :universities do |t|
+    #   t.string :name
+    #   t.text :about
+    #   t.references :city, foreign_key: true
+    #   t.references :country, foreign_key: true
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
