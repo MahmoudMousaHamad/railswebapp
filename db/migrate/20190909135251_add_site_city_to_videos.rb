312c0e3e0844c74e87490aece77d458f5e4b7526
Mousa Hamad <mahmoudhamad@ou.edu>
2019-09-10 09:06:19 -0500

    .

--- db/migrate/20190909135251_add_site_city_to_videos.rb
+++ db/migrate/20190909135251_add_site_city_to_videos.rb
@@ -1,5 +1,6 @@
 class AddSiteCityToVideos < ActiveRecord::Migration[6.0]
   def change
-    add_reference :videos, :city, null: false, foreign_key: true
+    # add_reference :videos, :city, null: false, foreign_key: true
+    add_reference :videos, :city, foreign_key: true
   end
 end
