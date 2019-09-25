97a31ff1f73d6c8e412126d3f8f6e4b903b21d92
Mousa Hamad <mahmoudhamad@ou.edu>
2019-09-25 09:11:27 -0500

    removed redundant migration

--- db/migrate/20190924023314_add_country_to_country_references2.rb
+++
@@ -1,7 +0,0 @@
-class AddCountryToCountryReferences < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :country_references, :country
-    remove_column :country_references, :reference
-    add_reference :country_references, :country, foreign_key: true
-  end
-end
