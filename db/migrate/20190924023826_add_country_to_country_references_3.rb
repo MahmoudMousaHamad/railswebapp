2b60a6ded7c906724fd15585616079a43d2f5008
Mousa Hamad <mahmoudhamad@ou.edu>
2019-09-25 09:15:21 -0500

    .

--- db/migrate/20190924023826_add_country_to_country_references_3.rb
+++ db/migrate/20190924023826_add_country_to_country_references_3.rb
@@ -1,7 +1,5 @@
 class AddCountryToCountryReferences3 < ActiveRecord::Migration[6.0]
   def change
-    remove_column :country_references, :country
-    remove_column :country_references, :reference
     add_reference :country_references, :country, foreign_key: true
   end
 end
