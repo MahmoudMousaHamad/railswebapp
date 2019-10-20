1e46312a79088c0bc71f7be119e971e624a76d8b
Mousa Hamad <mahmoudhamad@ou.edu>
2019-10-20 18:42:12 -0500

    .

--- app/controllers/library_controller.rb
+++ app/controllers/library_controller.rb
@@ -21,48 +21,42 @@ class LibraryController < ApplicationController
         end
         if q
             @search_results = PgSearch.multisearch(q)
-            @search_results = @search_results.select { |r| content_types.include? r.searchable_type}
-            @search_results = @search_results.select do |r|
-                if r.searchable_type == "Journal"
-                    journal = Journal.find(r.searchable_id)
-                    result = false
-                    result = journal.coverageFrom >= from_year if from_year
-                    result = result && journal.coverageTo <= to_year if to_year != 0
-                    result = result && !(journal.subjects.map { |s| s.name } & params[:search][:subjects]).empty? if params[:search]
-                    return result
-                elsif r.searchable_type == "Book"
-                    book = Book.find(r.searchable_id)
-                    result = false
-                    result = book.year >= from_year if from_year
-                    result = result && book.year <= to_year if to_year != 0
-                    result = result && !(book.subjects.map { |s| s.name } & params[:search][:subjects]).empty? if params[:search]
-                    return result
+            @search_results = @search_results.select { |r| content_types.include? r.searchable_type }
+            @categorized_results = []
+            @search_results.each do |r|
+                if r.searchable_type == "Book"
+                    @categorized_results.push(Book.find(r.searchable_id))
+                elsif r.searchable_type == "Journal"
+                    @categorized_results.push(Journal.find(r.searchable_id))
                 elsif r.searchable_type == "AcademicPaper"
-                    paper = AcademicPaper.find(r.searchable_id)
-                    result = false
-                    result = paper.publication_year >= from_year if from_year
-                    result = result && paper.publication_year <= to_year if to_year != 0
-                    result = result && !(paper.subjects.map { |s| s.name } & params[:search][:subjects]).empty? if params[:search]
-                    return result
+                    @categorized_results.push(AcademicPaper.find(r.searchable_id))
                 end
             end
-            
-            # @search_results = @search_results.select { |i| i.searchable_type.name == "Journal" ? i.coverageTo.to_i <= to_year : i.year <= to_year } if to_year != 0
-            # @search_results = @search_results.select { |i| !(i.subjects.map { |s| s.name } & params[:search][:subjects]).empty? } if params[:search]
-
-            # @categorized_results = []
-            # @search_results.each do |r|
-            #     if content_types.include?(r.searchable_type) && r.searchable_type == "Book"
-            #         @categorized_results.push(Book.find(r.searchable_id))
-            #     elsif content_types.include?(r.searchable_type) && r.searchable_type == "Journal"
-            #         @categorized_results.push(Journal.find(r.searchable_id))
-            #     elsif content_types.include?(r.searchable_type) && r.searchable_type == "AcademicPaper"
-            #         @categorized_results.push(AcademicPaper.find(r.searchable_id))
-            #     end
-            # end
-            # @categorized_results = @categorized_results.select { |i| i.class.name == "Journal" ? i.coverageFrom.to_i >= from_year : i.year >= from_year } if from_year != 0
-            # @categorized_results = @categorized_results.select { |i| i.class.name == "Journal" ? i.coverageTo.to_i <= to_year : i.year <= to_year } if to_year != 0
-            # @categorized_results = @categorized_results.select { |i| !(i.subjects.map { |s| s.name } & params[:search][:subjects]).empty? } if params[:search]
+            if from_year
+                @categorized_results = @categorized_results.select do |i| 
+                    class_name = i.class.name
+                    if class_name == "Journal"
+                        i.coverageFrom.to_i >= from_year 
+                    elsif class_name == "Book"
+                        i.year >= from_year
+                    elsif class_name == "AcademicPaper"
+                        i.publication_year >= from_year
+                    end
+                end
+            end
+            if to_year
+                @categorized_results = @categorized_results.select do |i| 
+                    class_name = i.class.name
+                    if class_name == "Journal"
+                        i.coverageTo.to_i <= to_year 
+                    elsif class_name == "Book"
+                        i.year <= to_year
+                    elsif class_name == "AcademicPaper"
+                        i.publication_year <= to_year
+                    end
+                end
+            end
+            @categorized_results = @categorized_results.select { |i| !(i.subjects.map { |s| s.name } & params[:search][:subjects]).empty? } if params[:search]
         end
     end
 
