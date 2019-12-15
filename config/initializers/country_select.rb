18fb89b17489eb44f63ecec3e6239e89cf29f0d9
Mousa Hamad <mmh@ou.edu>
2019-12-15 16:02:05 -0600

    removed .container and .content from all views

--- config/initializers/country_select.rb
+++ config/initializers/country_select.rb
@@ -2,10 +2,6 @@ CountrySelect::FORMATS[:with_data_attr] = lambda do |country|
     [
         country.name,
         country.name,
-        {
-            'data-country-code' => country.country_code,
-            'data-alpha3' => country.alpha3
-        }
     ]
   end
   
