d2a8ed88cc0aba9d7f2cf5cbf165b3f8ca176700
Mousa Hamad <mmh@ou.edu>
2019-12-16 11:31:19 -0600

    .

--- db/migrate/20191215191503_add_superadmin_to_user.rb
+++ db/migrate/20191215191503_add_superadmin_to_user.rb
@@ -1,8 +1,8 @@
 class AddSuperadminToUser < ActiveRecord::Migration[6.0]
   def change
-    remove_column :users, :role
-    remove_column :users, :country
-    remove_column :users, :is_verified
+    # remove_column :users, :role
+    # remove_column :users, :country
+    # remove_column :users, :is_verified
 
     add_column :users, :role, :string, :null => "member", :default => "member"
     add_column :users, :country, :string
