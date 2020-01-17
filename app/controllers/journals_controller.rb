8970052bb42b2881b71da026f4b638869f7a87de
Mousa Hamad <mmh@ou.edu>
2020-01-17 09:59:11 -0600

    Fixed Journals page v2

--- app/controllers/journals_controller.rb
+++ app/controllers/journals_controller.rb
@@ -4,7 +4,7 @@ class JournalsController < ApplicationController
             if params[:language] == "arabic"
                 @journals_hash = Journal.where.not(arabic_title: [nil, ""]).order_by(:arabic_title).group_by{|u| u.arabic_title[0]}
             else
-                @journals_hash = Journal.where(arabic_title: [nil, ""]).order_by(:title).group_by{|u| u.title[0]}
+                @journals_hash = Journal.all.order_by(:title).group_by{|u| u.title[0]}
             end
         end
     end
