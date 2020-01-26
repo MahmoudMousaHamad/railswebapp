d48072d8ceda6f1b515f0822a6d2dde74051bede
Mousa Hamad <mmh@ou.edu>
2020-01-26 14:53:55 -0600

    .

--- db/migrate/20200122192055_add_published_and_user_id_to_islamic_periods.rb
+++ db/migrate/20200122192055_add_published_and_user_id_to_islamic_periods.rb
@@ -1,6 +1,6 @@
 class AddPublishedAndUserIdToIslamicPeriods < ActiveRecord::Migration[6.0]
   def change
-    add_column :islamic_periods, :published, :boolean
-    add_column :islamic_periods, :user_id, :integer
+    add_column :islamic_periods, :published, :boolean unless column_exists? :islamic_periods, :published
+    add_column :islamic_periods, :user_id, :integer unless column_exists? :islamic_periods, :user_id
   end
 end
