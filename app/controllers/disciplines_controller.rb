28189b41e286d746fb750a4730d684fbd4c4e700
Mousa Hamad <mmh@ou.edu>
2020-02-02 19:30:33 -0600

    .

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,7 +1,7 @@
 class DisciplinesController < ApplicationController
   breadcrumb "Library", :library_path
   def show
-    @discipline = Discipline.where(name: params[:name]).published.first
+    @discipline = Discipline.find_by(name: params[:name]).published
     breadcrumb @discipline.name, discipline_path(@discipline)
   end
 end
