fe38b23496d3f3d0b52fa9daf65cfcc97231b17e
Mousa Hamad <mmh@ou.edu>
2020-02-13 21:17:35 -0600

    fix controller

--- app/controllers/disciplines_controller.rb
+++ app/controllers/disciplines_controller.rb
@@ -1,7 +1,7 @@
 class DisciplinesController < ApplicationController
   breadcrumb "Library", :library_path
   def show
-    @discipline = Discipline.find_by(name: params[:name]).published
-    breadcrumb @discipline.name, discipline_path(@discipline)
+    @discipline = Discipline.find_by(name: params[:name], published: true)
+    breadcrumb @discipline.name, discipline_path(@discipline) if !@discipline.name
   end
 end
