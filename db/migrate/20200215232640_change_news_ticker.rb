ea7172fd89adf04617480b93d57021af4948e69e
Mousa Hamad <mmh@ou.edu>
2020-02-18 12:49:31 -0600

    rename a recent migration file

--- db/migrate/20200215232640_change_news_ticker.rb
+++ db/migrate/20200215232640_change_news_ticker.rb
@@ -1,4 +1,4 @@
-class ChangeWebsiteGeneric < ActiveRecord::Migration[6.0]
+class ChangeNewsTicker < ActiveRecord::Migration[6.0]
   def change
     rename_table :news_tickers, :website_generics
     add_column :website_generics, :about_us_content, :text
